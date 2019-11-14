1e871a47943ba15defa0796bbc584d4095adea8a
Luis Stiven Arteaga Solano <luis.arteaga@ceiba.com.co>
2019-11-14 17:05:43 -0500

    Cambios temporales en repositorio de Consulta

--- src/main/java/com/mitocode/repo/IConsultaRepo.java
+++ src/main/java/com/mitocode/repo/IConsultaRepo.java
@@ -1,9 +1,16 @@
 package com.mitocode.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.mitocode.model.Consulta;
 
 public interface IConsultaRepo extends JpaRepository<Consulta, Integer>{
+	
+	@Query("FROM Consulta")//no es necesario el SELECT
+	List<Consulta> buscar(String dni, String nombreCompleto);
+	
 
 }
