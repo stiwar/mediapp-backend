f9439dd3ddc9a3aaf86e2f24fc9004f95fb34d65
Stiven Arteaga <stivenartesol@gmail.com>
2019-11-16 19:39:46 -0500

    Nuevos cambios temporales en repositorio de Consulta

--- src/main/java/com/mitocode/repo/IConsultaRepo.java
+++ src/main/java/com/mitocode/repo/IConsultaRepo.java
@@ -1,16 +1,21 @@
 package com.mitocode.repo;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.mitocode.model.Consulta;
 
 public interface IConsultaRepo extends JpaRepository<Consulta, Integer>{
 	
-	@Query("FROM Consulta")//no es necesario el SELECT
-	List<Consulta> buscar(String dni, String nombreCompleto);
+	//desde el front, se envía en minúsculas y en el back en memoria con LOWER() convertimos lo que esté en BD a minúsculas, sino no hay match
+	@Query("FROM Consulta c WHERE c.paciente.dni = :dni OR LOWER(c.paciente.nombres) LIKE %:nombreCompleto% OR LOWER(c.paciente.apellidos) LIKE %:nombreCompleto%")//no es necesario el SELECT en JPQL
+	List<Consulta> buscar(@Param("dni") String dni, @Param("nombreCompleto") String nombreCompleto);
 	
+	@Query("FROM Consulta c WHERE c.fecha BETWEEN")
+	List<Consulta> buscarFecha(LocalDateTime); 
 
 }
