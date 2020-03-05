b887a1f0c8b97f531de3f80486789548c89ee518
Luis Stiven Arteaga Solano <luis.arteaga@ceiba.com.co>
2020-03-05 16:46:44 -0500

    Se finaliza los servicios de roles para la gestión de menús de acuerdo al rol del usuario

--- src/main/java/com/mitocode/model/Menu.java
+++ src/main/java/com/mitocode/model/Menu.java
@@ -28,27 +28,47 @@ public class Menu {
 	private String url;
 
 	@ManyToMany(fetch = FetchType.EAGER)
-	@JoinTable(name = "menu_rol", joinColumns = @JoinColumn(name = "id_menu", referencedColumnName = "idMenu"), inverseJoinColumns = @JoinColumn(name = "id_rol", ) )
+	@JoinTable(name = "menu_rol", joinColumns = @JoinColumn(name = "id_menu", referencedColumnName = "idMenu"), inverseJoinColumns = @JoinColumn(name = "id_rol", referencedColumnName = "idRol"))
 	private List<Rol> roles;
 
 	public Integer getIdMenu() {
 		return idMenu;
 	}
 
+	public void setIdMenu(Integer idMenu) {
+		this.idMenu = idMenu;
+	}
+
 	public String getIcono() {
 		return icono;
+	}
+
+	public void setIcono(String icono) {
+		this.icono = icono;
 	}
 
 	public String getNombre() {
 		return nombre;
 	}
 
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
 	public String getUrl() {
 		return url;
 	}
 
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
 	public List<Rol> getRoles() {
 		return roles;
+	}
+
+	public void setRoles(List<Rol> roles) {
+		this.roles = roles;
 	}
 
 }
